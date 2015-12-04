3e2a2a6b21c30cef286da0a29fd46dfbf8079e49
Angel Diaz <angeldm@gmail.com>
2015-12-04 01:14:46 +0100

    css law

---
+++ web/js/cookie-consent.js
@@ -0,0 +1,8 @@
+window.cookieconsent_options = {
+	"message":"Nos obligan a molestarte con la obviedad de que este sitio usa cookies.",
+ 	"dismiss":"cerrar",
+ 	"learnMore":"Más información.",
+ 	"link":"http://cifec.net/privacy-policy-cookie-restriction-mode",
+ 	"theme":"dark-bottom"
+ };
+ 
